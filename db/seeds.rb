2b2733c42da8fcd7fbe00c30547a4b0dc11f53a1
Paul McCarthy <paul.mccarthy@avant.com>
2018-03-21 15:46:02 -0500

    seed file for terrains

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+t = TerrainType.new
+t.name = 'plains'
+t.water_tile = false
+t.movement_multiplier = 1
+t.save
+
+t = TerrainType.new
+t.name = 'forest'
+t.water_tile = false
+t.movement_multiplier = 2
+t.save
+
+t = TerrainType.new
+t.name = 'water'
+t.water_tile = true
+t.movement_multiplier = 1
+t.save
