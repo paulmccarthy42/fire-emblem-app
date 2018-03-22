7b2f7d8557e8117f01f54709ae41fe2aac04d8d6
Paul McCarthy <phantasos42@gmail.com>
2018-03-21 19:46:27 -0500

    centrify seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-names = ['plains', 'forest', 'water']
-
-3.times do |x|
-  t = TerrainType.new
-  t.name = names[x]
-  t.water_tile = names[x] == 'water'
-  t.movement_multiplier = 1
-  t.movement_multiplier *= 2 if names[x] == 'forest'
-  t.save
-end
+Character.create!([
+  {name: "Fiora", character_class_id: 1, hp: 18, loyalty: "player", status: "normal"},
+  {name: "Fiorina", character_class_id: 1, hp: 17, loyalty: "player", status: "normal"},
+  {name: "Roland", character_class_id: 2, hp: 22, loyalty: "enemy", status: "normal"}
+])
+CharacterClass.create!([
+  {name: "Fighter", can_use_sword: false, can_use_lance: false, can_use_axe: true, can_use_bow: false, can_use_anima: false, can_use_dark_magic: false, can_use_light_magic: false, can_use_staves: false, movement: 6, flying: false},
+  {name: "Pegasus Knight", can_use_sword: false, can_use_lance: true, can_use_axe: false, can_use_bow: false, can_use_anima: false, can_use_dark_magic: false, can_use_light_magic: false, can_use_staves: false, movement: 8, flying: true}
+])
+CharacterStat.create!([
+  {character_id: 1, max_hp: 18},
+  {character_id: 2, max_hp: 17},
+  {character_id: 3, max_hp: 22}
+])
+TerrainType.create!([
+  {name: "water", movement_multiplier: 1, water_tile: true},
+  {name: "forest", movement_multiplier: 2, water_tile: false},
+  {name: "plains", movement_multiplier: 1, water_tile: false}
+])
