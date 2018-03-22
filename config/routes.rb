f29ccf8f71600974afb7cc8a108bb5a25c72a3b9
Paul McCarthy <paul.mccarthy@avant.com>
2018-03-21 22:07:37 -0500

    last of the typos

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   namespace :v1 do
     get "/characters" => "characters#index"
     patch "/characters" => "characters#batch_update"
-    get "/map" => "level#generate_map"
+    get "/map" => "levels#generate_map"
   end
 end
